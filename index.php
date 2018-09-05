91329101051b78513a27a44c1177ba1ad119a2ac
Hylema <gleb.shalaykin@yandex.ru>
2018-09-05 21:47:21 +0300

    ege

--- index.php
+++ index.php
@@ -32,6 +32,8 @@ session_start();
     </style>
 </head>
 <body>
+    
+    <b>Hello</b>
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="index.php">Blog</a>
