4e7a6228666ecb7874db9c49f5a2742cd27c6591
Victor <victor.kataev@gmail.com>
2018-09-07 13:34:07 +0200

    Error filed added

--- new-post.php
+++ new-post.php
@@ -80,7 +80,8 @@ session_start();
 </nav>
 <div class="new-post-dashboard">
     <div class="container">
-        <form action="publish.php" method="post">
+        <div class="error"><?php echo $_SESSION['error']; ?></div>
+        <form action="publish.php" method="post" enctype="multipart/form-data">
             <fieldset class="form-group">
                 <label for="post-title">Title</label>
                 <input type="text" class="form-control" id="post-title" name="post-title" placeholder="Enter your title">
@@ -88,6 +89,10 @@ session_start();
             <fieldset class="form-group">
                 <label for="post-content" class="sr-only">Content</label>
                 <textarea name="post-content" id="post-content" placeholder="Contents..." class="form-control"></textarea>
+            </fieldset>
+            <fieldset class="form-group">
+                <label for="post-image">Upload image</label>
+                <input type="file" class="form-control-file" name="post-image" id="post-image">
             </fieldset>
             <button type="submit" class="btn btn-success">Publish</button>
         </form>
