dc7da7172fb8602943677670523b65c8da0320e4
GoodGuyVictor <victor.kataev1@gmail.com>
2018-09-09 16:00:19 +0200

    Post is now printed via OOP rather than manually Refactoring object Post -> BlogPost

--- Timeline.php
+++ Timeline.php
@@ -10,10 +10,10 @@ namespace blog\timeline;
 
 
 use blog\db\Db;
-use blog\post\Post;
+use blog\post\BlogPost;
 
 require_once ("Db.php");
-require_once ("Post.php");
+require_once("BlogPost.php");
 
 class Timeline
 {
@@ -27,7 +27,7 @@ class Timeline
         $result = $this->db->sqlSelectQuery($sql);
         if($result !== null) {
             while ($row = $result->fetch()) {
-                $this->posts[] = new Post($row["id"], $row["title"], $row["content"], $row["published"], $row["image"]);
+                $this->posts[] = new BlogPost($row["id"], $row["title"], $row["content"], $row["published"], $row["image"]);
             }
         }
     }
@@ -38,10 +38,7 @@ class Timeline
 
         if(!empty($this->posts)){
             foreach($this->posts as $post) {
-                if($post->image != null)
-                    $output .= '<div class="post"><h2 class="post-title">'.$post->title.'</h2><div class="post-image"><img src="'.$post->image.'" alt="post image"></div><div class="post-content">'.$post->content.'</div></div>';
-                else
-                    $output .= '<div class="post"><h2 class="post-title">'.$post->title.'</h2><div class="post-content">'.$post->content.'</div></div>';
+                $output .= $post->printPost();
             }
         } else {
             $output = "No posts yet";
