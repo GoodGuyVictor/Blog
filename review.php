9218787d4bc21efa6d263e77746c3fedac59c8f8
GoodGuyVictor <victor.kataev1@gmail.com>
2018-09-10 14:31:05 +0200

    riview.php has included header.php file now

--- review.php
+++ review.php
@@ -18,6 +18,7 @@ if($_GET['post']) {
         $postContent = $row['content'];
         $postImage = $row['image'];
         $postPublished = $row['published'];
+        $_SESSION['page-title'] = $postTitle;
     }
 } else {
     echo 'Post not found: 404';
@@ -25,75 +26,7 @@ if($_GET['post']) {
 
 ?>
 
-<!doctype html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
-    <title><?php echo $postTitle; ?></title>
-    <style>
-        .login-profile-btn {
-            margin-right: 70px;
-        }
-
-        .dropdown-item:hover {
-            background: #FFC107;
-
-        }
-    </style>
-</head>
-<body>
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="index.php">Blog</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </li>
-        </ul>
-
-        <div class="login-profile-btn">
-            <?php if($_SESSION["logged_in"]): ?>
-                <div class="dropdown" id="profile-dropdown">
-                    <button class="btn btn-warning dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <?php echo $_SESSION["username"]; ?>
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <a class="dropdown-item" href="#">My profile</a>
-                        <a class="dropdown-item" href="new-post.php">New post</a>
-                        <a class="dropdown-item" href="logout.php">Log out</a>
-                    </div>
-                </div>
-            <?php else: ?>
-                <a href="login.php" id="login-button" class="btn btn-success">Log In</a>
-            <?php endif ?>
-        </div>
-    </div>
-</nav>
+<?php require 'header.php'; ?>
 <div class="container">
     <h2><?php echo $postTitle; ?></h2>
     <?php if($postImage): ?>
