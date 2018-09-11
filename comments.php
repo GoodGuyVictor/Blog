6c537b65b47506bd2aa7a403f51b12fa28dd15f1
GoodGuyVictor <victor.kataev1@gmail.com>
2018-09-11 12:10:22 +0200

    Implemented view comments.php

--- comments.php
+++ comments.php
@@ -1,7 +1,35 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: victo
- * Date: 9/10/2018
- * Time: 2:48 PM
- */
+<div class="create-comment">
+    <form action="publish-comment.php" method="post">
+        <fieldset class="form-group">
+            <label for="new-comment" class="sr-only">New comment</label>
+            <textarea class="form-control" name="new-comment" id="new-comment" placeholder="Leave a comment here"></textarea>
+        </fieldset>
+        <button type="submit" class="btn btn-success">Comment</button>
+    </form>
+</div>
+<div class="comments">
+    <?php
+    use blog\comment\Comment;
+    use blog\db\Db;
+
+    require_once ('Db.php');
+    require_once ('Comment.php');
+
+        $db = Db::getInstance();
+        $sql = "SELECT * FROM comment WHERE post_id = ".$_SESSION['post_id']." ORDER BY created DESC";
+        $result = $db->sqlSelectQuery($sql);
+        $comments = [];
+        if($result) {
+            while($row = $result->fetch()) {
+                $comments[] = new Comment($row['id'], $row['content'], $row['created'], $row['user_id'], $row['post_id']);
+            }
+
+            foreach ($comments as $comment) {
+                $comment->printComment();
+            }
+        } else {
+            echo 'No comments yet';
+        }
+
+    ?>
+</div>
