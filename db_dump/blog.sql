b160a070c4d04456d30acc259bcf2be567b5eab3
GoodGuyVictor <victor.kataev1@gmail.com>
2018-09-12 13:50:16 +0200

    Database update Added new column author_id in table post

--- db_dump/blog.sql
+++ db_dump/blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Сен 11 2018 г., 16:14
+-- Время создания: Сен 12 2018 г., 14:48
 -- Версия сервера: 5.6.38
 -- Версия PHP: 7.2.0
 
@@ -45,7 +45,9 @@ INSERT INTO `comment` (`id`, `content`, `created`, `user_id`, `post_id`) VALUES
 (6, '123', '2018-09-10 22:31:57', 7, 22),
 (7, 'Hello world', '2018-09-10 22:38:25', 7, 22),
 (8, '777', '2018-09-10 22:50:43', 8, 22),
-(9, 'cool\r\n', '2018-09-10 22:52:29', 8, 18);
+(9, 'cool\r\n', '2018-09-10 22:52:29', 8, 18),
+(10, 'Пост отстой', '2018-09-12 12:18:54', 7, 23),
+(11, 'Hello world', '2018-09-12 12:19:14', 7, 23);
 
 -- --------------------------------------------------------
 
@@ -58,20 +60,22 @@ CREATE TABLE `post` (
   `title` varchar(500) DEFAULT NULL,
   `content` text,
   `published` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `image` varchar(1000) DEFAULT NULL
+  `image` varchar(1000) DEFAULT NULL,
+  `author_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `post`
 --
 
-INSERT INTO `post` (`id`, `title`, `content`, `published`, `image`) VALUES
-(17, 'First post', 'content', '2018-09-07 14:33:57', 'images/nick-reynolds-788174-unsplash.jpg'),
-(18, 'Second post', 'Second post sample content', '2018-09-07 19:52:02', 'images/neil-soni-698207-unsplash.jpg'),
-(19, '11', '11', '2018-09-09 15:30:25', ''),
-(20, '444', '444', '2018-09-09 15:48:24', NULL),
-(21, '555', '5555', '2018-09-09 15:54:17', 'images/13811310550.jpg'),
-(22, 'New post', 'Post text', '2018-09-10 14:46:49', NULL);
+INSERT INTO `post` (`id`, `title`, `content`, `published`, `image`, `author_id`) VALUES
+(17, 'First post', 'content', '2018-09-07 14:33:57', 'images/nick-reynolds-788174-unsplash.jpg', NULL),
+(18, 'Second post', 'Second post sample content', '2018-09-07 19:52:02', 'images/neil-soni-698207-unsplash.jpg', NULL),
+(19, '11', '11', '2018-09-09 15:30:25', '', NULL),
+(20, '444', '444', '2018-09-09 15:48:24', NULL, NULL),
+(21, '555', '5555', '2018-09-09 15:54:17', 'images/13811310550.jpg', NULL),
+(22, 'New post', 'Post text', '2018-09-10 14:46:49', NULL, NULL),
+(23, 'Post title', 'fa;sldfkj;asdkfj;sadfasd\r\nfa\r\nsd\r\nfa\r\ns\r\ndfas\r\ndfdfkglj;sdfgklj;sdflgkjs;dflkgs', '2018-09-12 12:18:27', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -110,7 +114,8 @@ ALTER TABLE `comment`
 -- Индексы таблицы `post`
 --
 ALTER TABLE `post`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_AUTHOR` (`author_id`);
 
 --
 -- Индексы таблицы `user`
@@ -126,13 +131,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT для таблицы `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT для таблицы `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT для таблицы `user`
@@ -150,6 +155,12 @@ ALTER TABLE `user`
 ALTER TABLE `comment`
   ADD CONSTRAINT `post_id` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`),
   ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `FK_AUTHOR` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
