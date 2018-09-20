dc110d8550e52ae4889915491e88b2bfd9a4f04e
GoodGuyVictor <victor.kataev1@gmail.com>
2018-09-20 20:44:45 +0200

    Only logged in users can leave comments now

--- comments.php
+++ comments.php
@@ -1,13 +1,20 @@
 <div class="create-comment">
-    <form action="publish-comment.php" method="post">
-        <fieldset class="form-group">
-            <label for="new-comment" class="sr-only">New comment</label>
-            <textarea class="form-control" name="new-comment" id="new-comment" placeholder="Leave a comment here"></textarea>
-        </fieldset>
-        <button type="submit" class="btn btn-success">Comment</button>
-    </form>
+    <?php if($_SESSION['user_id']): ?>
+        <form action="publish-comment.php" method="post">
+            <fieldset class="form-group">
+                <label for="new-comment" class="sr-only">New comment</label>
+                <textarea class="form-control" name="new-comment" id="new-comment" placeholder="Leave a comment here"></textarea>
+            </fieldset>
+            <button type="submit" class="btn btn-success">Comment</button>
+        </form>
+    <?php else: ?>
+        <p><a href="login.php">Login</a> to leave a comment</p>
+    <?php endif; ?>
 </div>
+
+
 <div class="comments">
+    <h1>Comments</h1>
     <hr>
     <?php
     use blog\comment\Comment;
