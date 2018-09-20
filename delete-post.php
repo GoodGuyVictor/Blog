94b82edf9018809b609a4e62077672c95c98201c
GoodGuyVictor <victor.kataev1@gmail.com>
2018-09-20 18:05:55 +0200

    delete-post.php script implemented

--- delete-post.php
+++ delete-post.php
@@ -1,7 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: victo
- * Date: 9/20/2018
- * Time: 5:50 PM
- */
+    session_start();
+
+    require_once ('Db.php');
+    $db = \blog\db\Db::getInstance();
+
+    //deleting comments first
+    $sql = "DELETE FROM comment WHERE post_id =" . $_SESSION['post_id'];
+    $db->sqlQuery($sql);
+
+    //deleting post itself
+    $sql = "DELETE FROM post WHERE id =" . $_SESSION['post_id'];
+    $db->sqlQuery($sql);
+
+    header('Location: index.php');
+    exit;
